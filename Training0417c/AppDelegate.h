1ab7ff4d3aaae80769c07cfd11ffb69238f38887
cyko <cyko@yahoo-inc.com>
2018-04-17 11:30:50 +0800

    Initial Commit

---
+++ Training0417c/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Training0417c
+//
+//  Created by  Chih-Yu Ko on 2018/4/17.
+//  Copyright © 2018年 Oath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Training0417c/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Training0417c
+//
+//  Created by  Chih-Yu Ko on 2018/4/17.
+//  Copyright © 2018年 Oath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
